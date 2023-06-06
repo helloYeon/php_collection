569ff8dba4f631fc60df48f356145befb1db572b
YEON.JINUN / ヨン.ジヌン <gnom878@gmail.com>
2023-06-06 18:43:54 +0900

    add

--- src/code.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "vendor/autoload.php";
-
-// sample
-// https://blog.capilano-fw.com/?p=727
-
-$array = ['key' => 'value'];
-$collection = collect($array);
-var_dump($collection);
